fd90634c415ce088331fc610a4af9b9e704f160a
Sebastián M. Guevara <82485346+SebaG20xx@users.noreply.github.com>
2022-03-15 22:25:20 -0300

    Final update
    Bot rewritten and working

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import requests
 from time import sleep
 from bs4 import BeautifulSoup
+from rut_chile.rut_chile import format_rut_with_dots
 client = commands.Bot(command_prefix = ';')
 @client.event
 async def on_ready():
@@ -12,14 +13,32 @@ async def doxxname(ctx,arg):
     user = ctx.message.author
     name = arg.replace("_"," ")
     URL = "https://www.nombrerutyfirma.com/buscar"
-    inputfile = open('Historialname.txt', 'w')
-    inputfile.write(name)
+    historialname = open('historialname.txt', 'w')
+    namehistorial = name + "/n"
+    historialname.write(namehistorial)
     print("procesando nombre:", name)
     response = requests.get(URL, params={'term': name}).text
     soup = BeautifulSoup(response, 'html.parser')
     out = soup.find_all('td')
     address = out[3].text + " " + out[4].text
     rutdoxx = out[1].text
-    Doxxtotal = "Resultados: " + name + ", " + rutdoxx + ", " + address
+    print("RUT encontrado:", out[1].text)
+    print("dirección:", address)
+    Doxxtotal = "Resultados" + name + " ," + rutdoxx + " ," + address
     await ctx.send(Doxxtotal)
-client.run("INSERTE TOKEN AQUÍ")
+@client.command(aliases=['doxxruts','test'])
+async def doxxrut(ctx,arg):
+    user = ctx.message.author
+    rut = arg
+    URL = "https://www.nombrerutyfirma.com/rut"
+    historialrut = open('historialrut.txt', 'w')
+    ruthistorial = rut + "/n"
+    historialrut.write(ruthistorial)
+    response = requests.get(URL, params={'term': format_rut_with_dots(rut)}).text
+    soup = BeautifulSoup(response, 'html.parser')
+    out = soup.find_all('td')
+    address = out[3].text + " " + out[4].text
+    nombrerut = out[0].text
+    Doxxtotalrut = "Resultados: " + nombrerut + " ," + rut + " ," + address
+    await ctx.send(Doxxtotalrut)
+client.run("INSERT_TOKEN_HERE")
